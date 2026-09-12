d2f0e885576780f74a61cd6a56650c29d2768c5a
Seifalden ekramy Mohamed mokhtar <seifalden.2825052@stemelfayoum.moe.edu.eg>
2026-09-12 18:24:13 +0300

    Prebundle API server for Vercel functions

--- api/[...path].js
+++ api/[...path].js
@@ -1,3 +1,3 @@
-import app from "../artifacts/api-server/src/app.ts";
+import app from "../artifacts/api-server/dist/vercel-app.mjs";
 
 export default app;
--- artifacts/vertex-os/api/[...path].js
+++ artifacts/vertex-os/api/[...path].js
@@ -1,3 +1,3 @@
-import app from "../../api-server/src/app.ts";
+import app from "../../api-server/dist/vercel-app.mjs";
 
 export default app;
